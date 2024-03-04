ce8e18e4640ad1df6d105423db4ac5a4570635ac
mks3011 <bmb3011@gmail.com>
2024-03-04 21:41:30 +0300

    done

--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,29 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
 
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print(word.lower().count("а"))
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
+print(len([letter for letter in word if letter.lower() in "аоуыэеёиюя"]))
 
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
+print(sentence.count(" "))
 
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+for word in sentence.split():
+    print(word[0])
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+print(len(sentence.replace(' ', '')) / len(sentence.split()))
